ba506118b0fad559be33a707e2df84aca43520b9
karol <pokrzywnickikarol@gmail,com>
2026-03-05 13:49:13 +0100

    dodano akrualizacje wyswietlen o 100 w kazdym filmie

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -12,3 +12,10 @@ const AvgRaitingGenre = db.movies.aggregate([
     ])
 print(AvgRaitingGenre)
 
+
+print("aktualizacja wyswietlen o 100 w kazdym filmie")
+
+db.movies.updateMany({}, { $inc: { wyswietlenia: 100 } },)
+
+const updatedViews =  db.movies.find()
+print(updatedViews)
