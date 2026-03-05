4abb306ef5064228ae46e7eabcec37853dc10bc1
karol <pokrzywnickikarol@gmail,com>
2026-03-05 13:36:39 +0100

    dodano zapytanie wyswietlajace srednia ocene dla kazdego filmu

--- queries/analysis.mongodb.js
+++ queries/analysis.mongodb.js
@@ -1,6 +1,14 @@
 db = db.getSiblingDB('DBmovies')
 
 print("\n--- RAPORT ANALITYCZNY MONGOFLIX ---");
-const wszystko = db.movies.find()
-print(wszystko)
+
+
+print("Srednia cena dla kazdego gatunku")
+const AvgRaitingGenre = db.movies.aggregate([
+        {$lookup: {from: "raiting", localField: "tytul" ,foreignField: "tytul_filmu", as: "film_raiting"}},
+        {$unwind: "$film_raiting"},
+        {$unwind: "$gatunek"},
+        {$group: {_id: "$gatunek", avgRaiting: {$avg : "$film_raiting.ocena"}}},
+    ])
+print(AvgRaitingGenre)
 
